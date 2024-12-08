7ad5e69692afa11e700bd838de6e095f5595b159
AyomiCoder <alukostephen01@gmail.com>
2024-12-08 14:53:52 +0100

    unary

--- src/learnJava/FlowControl.java
+++ src/learnJava/FlowControl.java
@@ -3,13 +3,17 @@ package learnJava;
 public class FlowControl {
 
 	public static void main(String[] args){
-        int value1 = 1;
-        int value2 = 2;
-        int result;
-        boolean someCondition = true;
-        result = someCondition ? value2 : value1;
-
-        System.out.println(result);
+//        int value1 = 1;
+//        int value2 = 2;
+//        int result;
+//        boolean someCondition = true;
+//        result = someCondition ? value2 : value1;
+//        
+//        System.out.println(result);
+		  int i = 10;
+		  int n = i++ % 5;
+		  System.out.println(i);
+		  System.out.println(n);
     }
 
 }
