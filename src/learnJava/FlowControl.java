c80cced1117a92cedc734fe96da81fb2c2f6e7b8
AyomiCoder <alukostephen01@gmail.com>
2024-12-08 14:34:40 +0100

    tenary

--- src/learnJava/FlowControl.java
+++ src/learnJava/FlowControl.java
@@ -2,9 +2,14 @@ package learnJava;
 
 public class FlowControl {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args){
+        int value1 = 1;
+        int value2 = 2;
+        int result;
+        boolean someCondition = true;
+        result = someCondition ? value2 : value1;
 
-	}
+        System.out.println(result);
+    }
 
 }
