191023fceaf48d866b490f9000d7d2e3e7309b08
AyomiCoder <alukostephen01@gmail.com>
2024-12-08 15:44:09 +0100

    if-else

--- src/learnJava/FlowControl.java
+++ src/learnJava/FlowControl.java
@@ -10,10 +10,19 @@ public class FlowControl {
 //        result = someCondition ? value2 : value1;
 //        
 //        System.out.println(result);
-		  int i = 10;
-		  int n = i++ % 5;
-		  System.out.println(i);
-		  System.out.println(n);
+//		  int i = 10;
+//		  int n = i++ % 5;
+//		  System.out.println(i);
+//		  System.out.println(n);
+		  int cadence = 25;
+		  boolean isMoving = false;
+		  
+		  if (isMoving) {
+			cadence--;
+			System.out.println(cadence);
+		} else {
+			System.err.println("cannot apply breaks when not in motion");
+		}
     }
 
 }
